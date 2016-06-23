5538a8cca00e051afb3f04c766439300345f7b28
radutatomir <tatomir.radu@gmail.com>
2016-06-23 11:26:12 +0000

    Roads controller unit tests

--- public/app/roads/roads.controller.js
+++ public/app/roads/roads.controller.js
@@ -3,7 +3,13 @@
 
 angular.module('Roads').controller('RoadsController', ['RoadsService', '$scope', function(RoadsService, $scope) {
     var ctrl = this;
-    ctrl.limits = [{current: 0},{ current: 0},{current: 0}];
+    ctrl.limits = [{
+        current: 0
+    }, {
+        current: 0
+    }, {
+        current: 0
+    }];
     ctrl.slowRoads = [];
     ctrl.closedRoads = [];
     ctrl.roadsInWork = [];
@@ -16,35 +22,39 @@ angular.module('Roads').controller('RoadsController', ['RoadsService', '$scope',
     ctrl.weatherSlowedRoadsLimit = 0;
     ctrl.weatherClosedRoadsLimit = 0;
 
-    RoadsService.roads().then(function(response) {
-        ctrl.slowRoads = response.data.slowRoads.data;
-        ctrl.roadsInWork = response.data.inWorkRoads.data;
-        ctrl.closedRoads = response.data.closedRoads.data;
-        ctrl.weatherClosedRoads = response.data.weatherClosedRoads.data;
-        ctrl.weatherSlowedRoads = response.data.weatherSlowedRoads.data;
+    ctrl.init = function() {
+        RoadsService.roads().then(function(response) {
+            ctrl.slowRoads = response.data.slowRoads.data;
+            ctrl.roadsInWork = response.data.inWorkRoads.data;
+            ctrl.closedRoads = response.data.closedRoads.data;
+            ctrl.weatherClosedRoads = response.data.weatherClosedRoads.data;
+            ctrl.weatherSlowedRoads = response.data.weatherSlowedRoads.data;
 
-        ctrl.limits = [{
-            current: ctrl.slowRoadsLimit,
-            max: ctrl.slowRoads.length
-        }, {
-            current: ctrl.roadsInWorkLimit,
-            max: ctrl.roadsInWork.length
-        }, {
-            current: ctrl.closedRoadsLimit,
-            max: ctrl.closedRoads.length
-        }, {
-            current: ctrl.weatherClosedRoadsLimit,
-            max: ctrl.weatherClosedRoads.length
-        }, {
-            current: ctrl.weatherSlowedRoadsLimit,
-            max: ctrl.weatherSlowedRoads.length
-        }];
-    });
+            ctrl.limits = [{
+                current: ctrl.slowRoadsLimit,
+                max: ctrl.slowRoads.length
+            }, {
+                current: ctrl.roadsInWorkLimit,
+                max: ctrl.roadsInWork.length
+            }, {
+                current: ctrl.closedRoadsLimit,
+                max: ctrl.closedRoads.length
+            }, {
+                current: ctrl.weatherClosedRoadsLimit,
+                max: ctrl.weatherClosedRoads.length
+            }, {
+                current: ctrl.weatherSlowedRoadsLimit,
+                max: ctrl.weatherSlowedRoads.length
+            }];
+        });
+    }
+
+    ctrl.init();
 
 
     ctrl.load = function() {
         for (var i = 0; i < ctrl.limits.length; i++) {
-            if ( ctrl.limits[i].current >= ctrl.limits[i].max ) {
+            if (ctrl.limits[i].current >= ctrl.limits[i].max) {
                 continue;
             }
             ctrl.limits[i].current += 6;
@@ -52,7 +62,7 @@ angular.module('Roads').controller('RoadsController', ['RoadsService', '$scope',
             if (ctrl.limits[i].current <= ctrl.limits[i].max) {
                 break;
             }
-            
+
         }
     }
 
---
+++ public/test/roads/roads.controller.spec.js
@@ -0,0 +1,131 @@
+/*global inject, expect, spyOnAngularService*/
+describe('RoadsController', function() {
+    var ctrl;
+    var RoadsService;
+    var $rootScope;
+    var $scope;
+
+    beforeEach(function() {
+        module('Roads');
+
+        inject(function($injector) {
+            $rootScope = $injector.get('$rootScope');
+            // $scope = $rootScope.$new();
+            $scope = {
+                $apply : function() {}
+            };
+            RoadsService = $injector.get('RoadsService');
+            ctrl = $injector.get('$controller')('RoadsController', {
+                RoadsService: RoadsService,
+                $scope: $scope
+            });
+        });
+    });
+
+
+    describe('Initialization', function() {
+        it('Should instatiate local variables with default values', function() {
+            expect(ctrl.slowRoads).toEqual([]);
+            expect(ctrl.closedRoads).toEqual([]);
+            expect(ctrl.roadsInWork).toEqual([]);
+            expect(ctrl.weatherClosedRoads).toEqual([]);
+            expect(ctrl.weatherSlowedRoads).toEqual([]);
+        });
+
+        it('Should instantiate limit variables with default values', function() {
+            expect(ctrl.slowRoadsLimit).toEqual(6);
+            expect(ctrl.roadsInWorkLimit).toEqual(0);
+            expect(ctrl.closedRoadsLimit).toEqual(0);
+            expect(ctrl.weatherClosedRoadsLimit).toEqual(0);
+            expect(ctrl.weatherSlowedRoadsLimit).toEqual(0);
+        });
+    });
+
+    describe('Service calls', function() {
+        it('Should call RoadsService and get all values and initialize limits array', function() {
+            var roads = [{
+                a: 1
+            }, {
+                b: 2
+            }];
+            var response = {
+                data: {
+                    slowRoads: {
+                        data: roads
+                    },
+                    inWorkRoads: {
+                        data: roads
+                    },
+                    closedRoads: {
+                        data: roads
+                    },
+                    weatherClosedRoads: {
+                        data: roads
+                    },
+                    weatherSlowedRoads: {
+                        data: roads
+                    }
+                }
+            };
+            var roadsSpy = spyOnAngularService(RoadsService, 'roads', response);
+
+            ctrl.init();
+            expect(roadsSpy).toHaveBeenCalled();
+            expect(ctrl.closedRoads).toEqual(roads);
+            expect(ctrl.slowRoads).toEqual(roads);
+            expect(ctrl.roadsInWork).toEqual(roads);
+            expect(ctrl.weatherClosedRoads).toEqual(roads);
+            expect(ctrl.weatherSlowedRoads).toEqual(roads);
+
+            expect(ctrl.limits).toEqual([{
+                current: ctrl.slowRoadsLimit,
+                max: ctrl.slowRoads.length
+            }, {
+                current: ctrl.roadsInWorkLimit,
+                max: ctrl.roadsInWork.length
+            }, {
+                current: ctrl.closedRoadsLimit,
+                max: ctrl.closedRoads.length
+            }, {
+                current: ctrl.weatherClosedRoadsLimit,
+                max: ctrl.weatherClosedRoads.length
+            }, {
+                current: ctrl.weatherSlowedRoadsLimit,
+                max: ctrl.weatherSlowedRoads.length
+            }]);
+
+        });
+    });
+
+    describe('Action handlers', function() {
+        describe('Lazy loading function', function() {
+            it('Should increment only the first value less than it\'s limit', function() {
+                ctrl.limits = [{
+                    current: 6,
+                    max: 6
+                }, {
+                    current: 0,
+                    max: 7
+                }, {
+                    current: 0,
+                    max: 8
+                }, {
+                    current: 0,
+                    max: 9
+                }, {
+                    current: 0,
+                    max: 10
+                }];
+                ctrl.load();
+                
+                expect(ctrl.limits[0].current).toEqual(6);
+                expect(ctrl.limits[1].current).toEqual(6);
+                expect(ctrl.limits[2].current).toEqual(0);
+                expect(ctrl.limits[3].current).toEqual(0);
+                expect(ctrl.limits[4].current).toEqual(0);
+            });
+        });
+    });
+
+
+});
